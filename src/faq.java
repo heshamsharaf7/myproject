5610275bdd4d3e8680beb77a86f83369f20743e9
Hesham <hesham737689633@gmail.com>
2024-02-14 11:18:16 +0300

    add method in the faq class

---
+++ src/faq.java
@@ -0,0 +1,11 @@
+public class faq {
+
+    private  void addQ(String q)
+    {
+        System.out.println("q: "+ q);
+    }
+    private  void addA(String q)
+    {
+        System.out.println("q: "+ q);
+    }
+}
